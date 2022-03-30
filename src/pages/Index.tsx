17384b01d67b3af046f5d400e7a0faa0ab785c9a
postor <postor@gmail.com>
2022-03-30 14:53:15 +0800

    basic

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import RegisterForm from '../components/RegisterForm'
 
 const Index = ()=>{
   return <div>
-    {/* <Button variant="contained">请参考storybook</Button> */}
     <RegisterForm />
   </div>
 }
